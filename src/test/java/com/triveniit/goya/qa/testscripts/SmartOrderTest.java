f74ccd09651607ffc132dd13946a24d9e7775124
semmet <steveemmet@outlook.com>
2019-05-03 12:00:06 -0400

    testing new jenkins server

--- src/test/java/com/triveniit/goya/qa/testscripts/SmartOrderTest.java
+++ src/test/java/com/triveniit/goya/qa/testscripts/SmartOrderTest.java
@@ -19,7 +19,6 @@ public class SmartOrderTest extends ScriptBase {
         customersPage.selectButton();
         customersPage.alertConfirm();
         homePage.verifyCustomerInfo();
-        delayFor(2000);
         homePage.navigateToSmartOrderPage();
         smartOrderPage.verifySmartOrderURL();
     }
